63285b157f3137c23c314d39f8288250be334d7c
shreyakhetani <112071340+shreyakhetani@users.noreply.github.com>
2023-02-15 21:03:28 +0200

    changed to lowercase

--- php/menu.php
+++ php/menu.php
@@ -4,14 +4,14 @@ include "header.php" ?>
 
 <div class="row">
         <div class="column left">
-          <br><a class="menu" href="../html/Breakfast.html" target="iframe_a"><img id="img1" src="../images/Breakfast.jpg"
-              alt="Image" width="200px"></a><br><a class="menu" href="../html/Breakfast.html"
+          <br><a class="menu" href="../html/breakfast.html" target="iframe_a"><img id="img1" src="../images/breakfast.jpg"
+              alt="Image" width="200px"></a><br><a class="menu" href="../html/breakfast.html"
             target="iframe_a">BREAKFAST</a><br>
 
-          <a class="menu" href="../html/Lunch.html" target="iframe_a"><img id="img1" src="../images/Lunch.jpg" alt="Image"
-              width="200px"></a><a class="menu" href="../html/Lunch.html" target="iframe_a">LUNCH</a><br>
-          <a class="menu" href="../html/Dinner.html" target="iframe_a"><img id="img1" src="../images/Dinner.jpeg" alt="Image"
-              width="200px"></a><a class="menu" href="../html/Dinner.html" target="iframe_a">DINNER</a>
+          <a class="menu" href="../html/lunch.html" target="iframe_a"><img id="img1" src="../images/lunch.jpg" alt="Image"
+              width="200px"></a><a class="menu" href="../html/lunch.html" target="iframe_a">LUNCH</a><br>
+          <a class="menu" href="../html/dinner.html" target="iframe_a"><img id="img1" src="../images/dinner.jpeg" alt="Image"
+              width="200px"></a><a class="menu" href="../html/dinner.html" target="iframe_a">DINNER</a>
 
 
         </div>
@@ -19,10 +19,10 @@ include "header.php" ?>
         <div class="column center">
           <br><a class="menu" href="../html/salad.html" target="iframe_a"><img id="img1" src="../images/salad.jpg" alt="Image"
               width="200px"></a><a class="menu" href="../html/salad.html" target="iframe_a">SALADS</a>
-          <a class="menu" href="../html/Dessert.html" target="iframe_a"><img id="img1" src="../images/Dessert.jpg" alt="Image"
-              width="200px"></a><a class="menu" href="../html/Dessert.html" target="iframe_a">DESSERTS</a><br>
-          <a class="menu" href="../html/Drinks.html" target="iframe_a"><img id="img1" src="../images/drink.jpg" alt="Image"
-              width="200px"></a><a class="menu" href="../html/Drinks.html" target="iframe_a">DRINKS</a>
+          <a class="menu" href="../html/dessert.html" target="iframe_a"><img id="img1" src="../images/dessert.jpg" alt="Image"
+              width="200px"></a><a class="menu" href="../html/dessert.html" target="iframe_a">DESSERTS</a><br>
+          <a class="menu" href="../html/drinks.html" target="iframe_a"><img id="img1" src="../images/drink.jpg" alt="Image"
+              width="200px"></a><a class="menu" href="../html/drinks.html" target="iframe_a">DRINKS</a>
         </div>
         <!--end of column03-->
         <!--create column2-->
@@ -31,17 +31,17 @@ include "header.php" ?>
           <!--make a responsive-->
           <div class="Un-order">
             <ul>
-              <li><a class="menu" href="../html/Breakfast.html" target="iframe_a">BREAKFAST</a> </li>
-              <li class="min"><a class="menu" href="../html/Lunch.html" target="iframe_a">LUNCH </a> </li>
-              <li class="min1"><a class="menu" href="../html/Lunch.html" target="iframe_a">LUNCH &nbsp;</a> </li>
-              <li class="min" id="line1">&nbsp;&nbsp;&nbsp;<a class="menu" href="../html/Dinner.html"
+              <li><a class="menu" href="../html/breakfast.html" target="iframe_a">BREAKFAST</a> </li>
+              <li class="min"><a class="menu" href="../html/lunch.html" target="iframe_a">LUNCH </a> </li>
+              <li class="min1"><a class="menu" href="../html/lunch.html" target="iframe_a">LUNCH &nbsp;</a> </li>
+              <li class="min" id="line1">&nbsp;&nbsp;&nbsp;<a class="menu" href="../html/dinner.html"
                   target="iframe_a">DINNER</a> </li>
-              <li class="min1" id="line1"><a class="menu" href="../html/Dinner.html" target="iframe_a">DINNER</a> </li>
+              <li class="min1" id="line1"><a class="menu" href="../html/dinner.html" target="iframe_a">DINNER</a> </li>
             </ul>
             <ul>
               <li id="line"><a class="menu" href="../html/salad.html" target="iframe_a">SALADS</a></li>
-              <li><a class="menu" href="../html/Dessert.html" target="iframe_a">DESSERTS</a></li>
-              <li><a class="menu" href="../html/Drinks.html" target="iframe_a">DRINKS</a>&nbsp;</li>
+              <li><a class="menu" href="../html/dessert.html" target="iframe_a">DESSERTS</a></li>
+              <li><a class="menu" href="../html/drinks.html" target="iframe_a">DRINKS</a>&nbsp;</li>
             </ul>
           </div>
           <!--add a iframe-->
