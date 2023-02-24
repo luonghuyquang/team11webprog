1f8d3a5dc6c3e92defad897171634ea0a3644047
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-24 06:47:48 +0200

    database_update

--- database/team11.sql
+++ database/team11.sql
@@ -219,6 +219,40 @@ ALTER TABLE `shreya_review`
   ADD CONSTRAINT `shreya_review_ibfk_1` FOREIGN KEY (`email_address`) REFERENCES `aisha_customer` (`email_address`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
+DROP TABLE IF EXISTS quang_tables, quang_reservation, quang_reserved;
+CREATE TABLE quang_tables (
+  table_id VARCHAR(50) NOT NULL PRIMARY KEY,
+  seats INT,
+  table_name VARCHAR(30),
+  table_spec VARCHAR(40)
+);
+INSERT INTO quang_tables (table_id, seats, table_name, table_spec)
+VALUES ('meeting_room_1', 10, 'Meeting Room 1', 'Good for Business and Event'),
+       ('table_1', 2, 'Table 1', 'Good for Friends'),
+       ('table_2', 2, 'Table 2', 'Good for Dating'),
+       ('table_3', 2, 'Table 3', 'Good for Singles'),
+       ('table_4', 4, 'Table 4', 'Good for Friends'),
+       ('table_5', 4, 'Table 5', 'Good for Business Group'),
+       ('table_6', 6, 'Table 6', 'Good for Business Group')
+;
+
+CREATE TABLE quang_reservation (
+  reservation_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  reservation_code VARCHAR(6) NULL,
+  cust_name VARCHAR(30) NOT NULL,
+  phone_nbr VARCHAR(20) NOT NULL,
+  cust_email VARCHAR(50) NOT NULL,
+  FOREIGN KEY (cust_email) REFERENCES aisha_customer(email_address),
+  start_datetime DATETIME,
+  end_datetime DATETIME,
+  nbr_of_guests INT NOT NULL,
+  special_request VARCHAR(255),
+  table_id VARCHAR(20) NOT NULL,
+  FOREIGN KEY (table_id) REFERENCES quang_tables(table_id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
