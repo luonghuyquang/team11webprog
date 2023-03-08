53b2f4951f2577d27704f10503dae526479fbdc4
shreyakhetani <112071340+shreyakhetani@users.noreply.github.com>
2023-03-08 12:12:26 +0200

    review page changes

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,18 +1,46 @@
 
-  function val(){
-     let username=document.loginform.username.value;
-     let password=document.loginform.pass.value;
-     if(username.length==null){
-      if(password==null){
-       alert("logged successfully!");
-        return false;
-      }else{
-        window.alert("Invalid username and password!");
-       return false;
-      }
-     }
-     else{
-       alert("Invalid username and password!");
-       return true;
-     }
+function val() {
+  let username = document.loginform.username.value;
+  let password = document.loginform.pass.value;
+  if (username.length == null) {
+    if (password == null) {
+      alert("logged successfully!");
+      return false;
+    } else {
+      window.alert("Invalid username and password!");
+      return false;
     }
+  }
+  else {
+    alert("Invalid username and password");
+    return true;
+  }
+}
+
+// javscript validation for rview
+
+function fnameVal() {
+  let fname = (document.reviewForm.fname.value).trim();
+  if (fname.length < 3) {
+    alert("First name must have atleast 3 characters.");
+    return false;
+  }
+
+}
+
+function lnameVal() {
+  let lname = (document.reviewForm.lname.value).trim();
+  if (lname.length < 1) {
+    alert("Last name cannot be empty.");
+    return false;
+  }
+
+}
+
+function reviewValidation() {
+  let rate = (document.reviewForm.rate.value).trim();
+  if (rate === '') {
+    alert("Rate is required.");
+    return false;
+  }
+}
--- php/db.php
+++ php/db.php
@@ -1,13 +1,13 @@
 <?php
-$servername='d4cab9dc2e30';
+$servername='db';
 $username='team11';
 $password='j62JL!C4)WrPn3Zp';
 $dbname='team11';
 //creating connection
-$conn =mysqli_connect($servername, $username, $password,$dbname);
+$conn = new mysqli ($servername, $username, $password, $dbname);
 //test connection
 if($conn->connect_error){
- die("connection failed:".$conn->connect_error);
+ die("connection failed:" . $conn->connect_error);
 
 }
 ?>
