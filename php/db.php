4730dac8f8eae05cf1eccdc07ef8ad5a0e225f14
luonghuyquang <luonghuyquang092022@gmail.com>
2023-03-09 13:17:30 +0200

    db and header

--- php/db.php
+++ php/db.php
@@ -4,13 +4,9 @@ $username='team11';
 $password='j62JL!C4)WrPn3Zp';
 $dbname='team11';
 //creating connection
-$conn =mysqli_connect($servername, $username, $password,$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 //test connection
 if($conn->connect_error){
- die("connection failed:".$conn->connect_error);
-
-// check the connection
-if ($conn->connect_error){
-    die("connection failed" . $conn->connect_error);
+ die("connection failed:" . $conn->connect_error);
 }
 ?>
